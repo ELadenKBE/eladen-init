d0754d2845987b3888c60e2788dca00ceb4eba9b
tb <tb@home-klick.de>
2023-07-31 11:52:47 +0200

    update config for keycloak

--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Function to clone a repository and check if it was successful
+# Function to clone a repository if it does not exist and check if it was successful
 clone_repository() {
   local repo_url="$1"
   local repo_name="${repo_url##*/}"
   repo_name="${repo_name%.git}"
+
+  if [ -d "$repo_name" ]; then
+    echo "Repository $repo_name already exists. Skipping..."
+    return 0
+  fi
 
   git clone "$repo_url" || { echo "Failed to clone $repo_url"; exit 1; }
 
@@ -38,6 +43,8 @@ cd eladen-api-gateway || { echo "Failed to change directory to eladen-api-gatewa
 cd ..
 
 # Execute docker-compose up from the root directory
-docker-compose up -d
+docker-compose -f docker-compose-services.yaml up -d
+cd keycloak
+docker-compose -f docker-compose-keycloak.yaml up -d
 
 echo "All repositories cloned and docker-compose up executed successfully!"
